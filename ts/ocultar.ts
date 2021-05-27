3cf66e8883f733f44693825454b5ac95eae7a43f
ClaraFelix <clara.patriciafelix@hotmail.com>
2021-05-27 17:05:02 -0300

    Estrutura inicial do TypeScript #3

---
+++ ts/ocultar.ts
@@ -0,0 +1,16 @@
+var cadastro = document.getElementById('btmcad');
+cadastro.onclick = function() {
+    document.getElementById("cadastro").style.visibility = "visible";
+    document.getElementById("cadastro").style.display = "flex";
+    document.getElementById("entrar").style.visibility = "hidden";
+    document.getElementById("entrar").style.display = "none";
+
+} 
+var entrar = document.getElementById('btmlogin');
+entrar.onclick = function() {
+    document.getElementById("cadastro").style.visibility = "hidden";
+    document.getElementById("cadastro").style.display = "none";
+    document.getElementById("entrar").style.visibility = "visible";
+    document.getElementById("entrar").style.display = "flex";
+
+} 
